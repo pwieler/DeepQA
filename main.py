fd42ecd900710f4d3f40fb439a38f16b73f4b608
pwieler <pwieler@users.noreply.github.com>
2018-01-16 03:08:21 +0100

    added grid_search

--- main.py
+++ main.py
@@ -257,14 +257,37 @@ def test():
     return test_loss_history, accuracy
 
 
+class GridSearch():
+    def __init__(self):
+        self.embeddings = [5,10,20,30,40,50]
+        self.story_hiddens = [5,10,20,30,40,50]
+        self.layers = [1,2,3,4]
+        self.batch_sizes = [32]
+        self.learning_rates = [0.001,0.0001]
+        self.params = []
+
+    def generateParamSet(self):
+
+        self.params=[]
+
+        for b in self.batch_sizes:
+            for lr in self.learning_rates:
+                for l in self.layers:
+                    for s in self.story_hiddens:
+                        for e in self.embeddings:
+                            self.params.append([e,s,l,b,lr])
+
+        return self.params
+
+
 
 if __name__ == "__main__":
 
     ## Load data
     data_path = "data/"
 
-    challenge = 'tasks_1-20_v1-2/shuffled/qa1_single-supporting-fact_{}.txt'
-    #challenge = 'tasks_1-20_v1-2/en/qa2_two-supporting-facts_{}.txt'
+    #challenge = 'tasks_1-20_v1-2/shuffled/qa1_single-supporting-fact_{}.txt'
+    challenge = 'tasks_1-20_v1-2/en/qa2_two-supporting-facts_{}.txt'
     #challenge = 'tasks_1-20_v1-2/en/qa3_three-supporting-facts_{}.txt'
     #challenge = 'tasks_1-20_v1-2/en/qa6_yes-no-questions_{}.txt'
 
@@ -295,80 +318,83 @@ if __name__ == "__main__":
     train_dataset = QADataset(x,xq,y,xl,xql)
     test_dataset = QADataset(tx,txq,ty,txl,txql)
 
-
-
-
-    ## Parameters
-    EMBED_HIDDEN_SIZE = 50
-    STORY_HIDDEN_SIZE = 50
-    QUERY_HIDDEN_SIZE = 50  # note: since we are adding the encoded query to the embedded stories,
-    #  QUERY_HIDDEN_SIZE should be equal to EMBED_HIDDEN_SIZE
-    N_LAYERS = 1
-    BATCH_SIZE = 32
-    EPOCHS = 40
-    VOC_SIZE = vocab_size
-    LEARNING_RATE = 0.001
-
+    g = GridSearch()
+    params = g.generateParamSet()
 
     PLOT_LOSS = False
     PRINT_LOSS = False
 
-    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    for i, param_set in enumerate(params):
+
+        print('\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nParam-Set: %d of %d' %(i,len(params)))
+
+        ## Parameters
+        EMBED_HIDDEN_SIZE = param_set[0]
+        STORY_HIDDEN_SIZE = param_set[1]
+        QUERY_HIDDEN_SIZE = param_set[0]  # note: since we are adding the encoded query to the embedded stories,
+        #  QUERY_HIDDEN_SIZE should be equal to EMBED_HIDDEN_SIZE
+        N_LAYERS = param_set[2]
+        BATCH_SIZE = param_set[3]
+        EPOCHS = 50
+        VOC_SIZE = vocab_size
+        LEARNING_RATE = param_set[4]
+
+        train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+        test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 
-    ## Initialize Model and Optimizer
-    model = QAModel(VOC_SIZE, EMBED_HIDDEN_SIZE, STORY_HIDDEN_SIZE, QUERY_HIDDEN_SIZE, VOC_SIZE, N_LAYERS)
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    criterion = nn.NLLLoss()
+        ## Initialize Model and Optimizer
+        model = QAModel(VOC_SIZE, EMBED_HIDDEN_SIZE, STORY_HIDDEN_SIZE, QUERY_HIDDEN_SIZE, VOC_SIZE, N_LAYERS)
+        optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+        criterion = nn.NLLLoss()
 
 
-    ## Print setting
-    print('Challenge selected: %s' %challenge.split('_')[3])
-    print('\nSettings:\nEMBED_HIDDEN_SIZE: %d\nSTORY_HIDDEN_SIZE: %d\nQUERY_HIDDEN_SIZE: %d'
-          '\nN_LAYERS: %d\nBATCH_SIZE: %d\nEPOCHS: %d\nVOC_SIZE: %d\nLEARNING_RATE: %f\n\n'
-          %(EMBED_HIDDEN_SIZE,STORY_HIDDEN_SIZE,QUERY_HIDDEN_SIZE,N_LAYERS,BATCH_SIZE,EPOCHS,VOC_SIZE,LEARNING_RATE))
-    print(model)
+        ## Print setting
+        print('Challenge selected: %s' %challenge.split('_')[3])
+        print('\nSettings:\nEMBED_HIDDEN_SIZE: %d\nSTORY_HIDDEN_SIZE: %d\nQUERY_HIDDEN_SIZE: %d'
+              '\nN_LAYERS: %d\nBATCH_SIZE: %d\nEPOCHS: %d\nVOC_SIZE: %d\nLEARNING_RATE: %f\n'
+              %(EMBED_HIDDEN_SIZE,STORY_HIDDEN_SIZE,QUERY_HIDDEN_SIZE,N_LAYERS,BATCH_SIZE,EPOCHS,VOC_SIZE,LEARNING_RATE))
+        #print(model)
 
 
-    ## Start training
-    start = time.time()
-    if PRINT_LOSS:
-        print("Training for %d epochs..." % EPOCHS)
+        ## Start training
+        start = time.time()
+        if PRINT_LOSS:
+            print("Training for %d epochs..." % EPOCHS)
 
-    train_loss_history = []
-    test_loss_history = []
+        train_loss_history = []
+        test_loss_history = []
 
-    train_acc_history = []
-    test_acc_history = []
+        train_acc_history = []
+        test_acc_history = []
 
-    for epoch in range(1, EPOCHS + 1):
+        for epoch in range(1, EPOCHS + 1):
 
-        print("Epoche: %d" %epoch)
-        # Train cycle
-        train_loss, train_accuracy, total_loss = train()
+            print("Epoche: %d" %epoch)
+            # Train cycle
+            train_loss, train_accuracy, total_loss = train()
 
-        # Test cycle
-        test_loss, test_accuracy = test()
+            # Test cycle
+            test_loss, test_accuracy = test()
 
-        # Add Loss to history
-        train_loss_history = train_loss_history+train_loss
-        test_loss_history = test_loss_history+test_loss
+            # Add Loss to history
+            train_loss_history = train_loss_history+train_loss
+            test_loss_history = test_loss_history+test_loss
 
-        # Add Loss to history
-        train_acc_history.append(train_accuracy) # = train_acc_history + [train_accuracy]
-        test_acc_history.append(test_accuracy) # = test_acc_history + test_accuracy
+            # Add Loss to history
+            train_acc_history.append(train_accuracy) # = train_acc_history + [train_accuracy]
+            test_acc_history.append(test_accuracy) # = test_acc_history + test_accuracy
 
-    # Plot Loss
-    if PLOT_LOSS:
-        plt.figure()
-        plt.plot(train_loss_history, label='train-loss', color='b')
-        plt.plot(test_loss_history, label='test-loss', color='r')
-        plt.legend()
-        plt.show()
+        # Plot Loss
+        if PLOT_LOSS:
+            plt.figure()
+            plt.plot(train_loss_history, label='train-loss', color='b')
+            plt.plot(test_loss_history, label='test-loss', color='r')
+            plt.legend()
+            plt.show()
 
-        plt.figure()
-        plt.plot(train_acc_history, label='train-accuracy', color='b')
-        plt.plot(test_acc_history, label='test-accuracy', color='r')
-        plt.legend()
-        plt.show()
+            plt.figure()
+            plt.plot(train_acc_history, label='train-accuracy', color='b')
+            plt.plot(test_acc_history, label='test-accuracy', color='r')
+            plt.legend()
+            plt.show()
