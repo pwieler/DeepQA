d2af463063149fd076a326c780b77100bf8d9913
pwieler <pwieler@users.noreply.github.com>
2018-01-21 23:15:26 +0100

    comments

--- DeepQA.py
+++ DeepQA.py
@@ -5,14 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import time
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
 
-# Some utility functions
+
+## Preprocessing of Babi-Data --> this just works for the Sentence-Model!
+
 def time_since(since):
     s = time.time() - since
     m = math.floor(s / 60)
@@ -87,7 +88,6 @@ def pad_sequences(sequences, maxlen=None, dtype='int32',
             raise ValueError('Padding type "%s" not understood' % padding)
     return x
 
-
 def parse_stories(lines, only_supporting=False):
     '''Parse stories provided in the bAbi tasks format
 
@@ -120,7 +120,6 @@ def parse_stories(lines, only_supporting=False):
             story.append(sent)
     return data
 
-
 def get_stories(f, only_supporting=False, max_length=None):
     '''Given a file name, read the file, retrieve the stories,
     and then convert the sentences into a single story.
@@ -138,8 +137,6 @@ def get_stories(f, only_supporting=False, max_length=None):
         #else:
         #    data.append((story[len(story)-max_length:len(story)], q, answer))
     return data1+data2
-
-
 
 def tokenize(sent):
     '''Return the tokens of a sentence including punctuation.
@@ -178,7 +175,6 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen, fact_maxlen):
 
     return pad_sequences(xs, maxlen=story_maxlen, padding='post'), pad_sequences(xqs, maxlen=query_maxlen, padding='post'), np.array(ys), np.array(xsl), np.array(xqsl), pad_sequences(facts_lengths, maxlen=story_maxlen, padding='post') # info pad_sequence wurde in rnn.py reinkopiert
 
-
 class QADataset(Dataset):
 
     def __init__(self, story, query, answer, story_lengths, query_lengths, facts_lengths):
@@ -196,10 +192,21 @@ class QADataset(Dataset):
     def __len__(self):
         return self.len
 
-class QAModel(nn.Module):
-    def __init__(self, input_size, embedding_size, story_hidden_size, query_hidden_size, output_size, n_layers=1, bidirectional=False):
-        super(QAModel, self).__init__()
 
+## Model with separated facts
+#  Remember:
+#       Our dataset consists out of stories and queries --> every story belongs to one query
+#       --> every story consists out of multiple facts
+#       --> one fact is one sentence: "Mary is in the house."
+#  This is model, where all the facts for one query are read in separateley instead of reading in all words together!
+#  The intention was that it may perform better because it has more information about one unique fact!
+#  At the moment it does not outperform our standard model --> but here it is working progress!
+#
+class SentenceModel(nn.Module):
+    def __init__(self, input_size, embedding_size, story_hidden_size, query_hidden_size, output_size, n_layers=1, bidirectional=False):
+        super(SentenceModel, self).__init__()
+
+        ## Definition of Input- & Output-Sizes
         self.voc_size = input_size
         self.embedding_size = embedding_size
         self.story_hidden_size = story_hidden_size
@@ -207,19 +214,25 @@ class QAModel(nn.Module):
         self.n_layers = n_layers
         self.n_directions = int(bidirectional) + 1
 
-        self.story_embedding = nn.Embedding(input_size, embedding_size) #Embedding bildet ab von Vokabular (Indize) auf n-dim Raum
+        ## Definition of Embeddings
+        self.story_embedding = nn.Embedding(input_size, embedding_size)
 
-        self.story_rnn = nn.GRU(embedding_size, story_hidden_size, n_layers,
-                                bidirectional=bidirectional, batch_first=True, dropout=0.4)
+        self.query_embedding = nn.Embedding(input_size, embedding_size)
 
+        ## Definition of RNNs --> we have three different GRUs
+        # Reads in each word of one fact --> generates a encoding for all the facts belonging to one query
         self.fact_rnn = nn.GRU(embedding_size, story_hidden_size, n_layers,
                                 bidirectional=bidirectional, batch_first=True, dropout=0.4)
 
-        self.query_embedding = nn.Embedding(input_size, embedding_size)
+        # Reads in all fact-encodings belonging to one query --> generates one encoding for the whole story that is related to the query!
+        self.story_rnn = nn.GRU(embedding_size, story_hidden_size, n_layers,
+                                bidirectional=bidirectional, batch_first=True, dropout=0.4)
+
+        # Reads in each word of the query --> generates one encoding for the query
         self.query_rnn = nn.GRU(embedding_size, query_hidden_size, n_layers,
                                 bidirectional=bidirectional, batch_first=True, dropout=0.4)
 
-        # info: if we use the old-forward function fc-layer has input-length: "story_hidden_size+query_hidden_size"
+        ## Definition of Output-Layers --> here we do softmax on the vocabulary_size!
         self.fc1 = nn.Linear(story_hidden_size, 300)
         self.dr1 = nn.Dropout(p=0.4)
         self.fc2 = nn.Linear(300, 250)
@@ -230,22 +243,25 @@ class QAModel(nn.Module):
 
     def forward(self, story, query, story_lengths, query_lengths, fact_lengths):
 
-        #story: 32x20x7
-        #query: 32x5
-        #story_lengths: 32
-        #query_lengths: 32
-        #fact_lengths: 32x20fact_maxlen
 
-        # Calculate Batch-Size
+        #story: BATCH_SIZE x STORY_MAX_LEN x FACT_MAX_LEN
+        #query: BATCH_SIZE x QUERY_MAX_LEN
+        #story_lengths: contains the number of facts each story has
+        #query_lengths: just needed to do packing/unpacking of queries --> this is not done at the moment
+        #fact_lengths: just needed to do packing/unpacking of facts --> this is not done at the moment
+
+        # FACT_MAX_LEN is the maximum size of one fact (maximum amount of words + padding)
+
+        # Determine Batch-Size
         batch_size = story.size(0)
         story_size = story.size(1)
 
-        # Make a hidden
+        # Make hidden for the GRUs
         fact_hidden = self._init_hidden(batch_size*story_size, self.story_hidden_size)
         story_hidden = self._init_hidden(batch_size, self.story_hidden_size)
         query_hidden = self._init_hidden(batch_size, self.query_hidden_size)
 
-        # Embed query
+        # Embed Query-Words
         q_e = self.query_embedding(query)
         # Encode query-sequence with RNN
         query_output, query_hidden = self.query_rnn(q_e, query_hidden)
@@ -256,15 +272,22 @@ class QAModel(nn.Module):
         # and can forget unnecessary information!
         question_code = query_hidden[0]
         question_code = question_code.view(batch_size,1,self.query_hidden_size)
+
+        # Create a question-code for every word!
         question_code_words = question_code.view(batch_size,1,1,self.query_hidden_size).repeat(1, story.size(1), fact_maxlen, 1)
+        # Create a question-code for every fact!
         question_code_facts = question_code.repeat(1,story.size(1),1)
 
-        # Embed story
+        # Embed Words that are contained in the story
+        # --> to do that we have to rearrange the tensor, so that we have the form:
+        #       Batch_size x #Words
         s_e = self.story_embedding(story.view(batch_size,story_size*fact_maxlen))
         s_e = s_e.view(batch_size,story_size,fact_maxlen,-1) # 32x20x7x50
 
+        # Combine word-embeddings with question_code
         s_e = s_e + question_code_words
 
+        # Read in the words belonging to the facts into the Fact-RNN --> generate fact-encodings
         fact_output, fact_hidden = self.fact_rnn(s_e.view(batch_size*story_size,fact_maxlen,-1), fact_hidden)
 
         fact_encodings = fact_hidden.view(batch_size, story_size, -1) # 32x20x50
@@ -293,6 +316,9 @@ class QAModel(nn.Module):
         hidden = torch.zeros(self.n_layers * self.n_directions,
                              batch_size, hidden_size)
         return Variable(hidden)
+
+
+
 
 # Train cycle
 def train():
@@ -348,11 +374,12 @@ def train():
 
     accuracy = 100. * correct / train_data_size
 
-    #if PRINT_LOSS:
     print('Training set: Accuracy: {}/{} ({:.0f}%)'.format(
         correct, train_data_size, accuracy))
 
     return train_loss_history, accuracy, total_loss  # loss per epoch
+
+
 
 def test():
 
@@ -377,7 +404,6 @@ def test():
         # Sort stories by their length
         sl, perm_idx = sl.sort(0, descending=True)
         stories = stories[perm_idx]
-        #ql, perm_idx = ql.sort(0, descending=True) # if we sort query also --> then they do not fit together!
         fl = fl[perm_idx]
         ql = ql[perm_idx]
         queries = queries[perm_idx]
@@ -393,7 +419,6 @@ def test():
 
     accuracy = 100. * correct / test_data_size
 
-    #if PRINT_LOSS:
     print('Test set: Accuracy: {}/{} ({:.0f}%)'.format(
         correct, test_data_size, accuracy))
 
@@ -425,10 +450,10 @@ vocab = sorted(vocab)
 # Reserve 0 for masking via pad_sequences
 # Vocabluary Size
 vocab_size = len(vocab) + 1
-#Creates Dictionary
+# Creates Dictionary
 word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
 
-#Max Length of Story and Query
+# Max Length of Story and Query
 story_maxlen = max(map(len, (x for x, _, _ in train_data + test_data)))
 query_maxlen = max(map(len, (x for _, x, _ in train_data + test_data)))
 fact_maxlen = 7
@@ -442,7 +467,7 @@ N_LAYERS = 1
 BATCH_SIZE = 32
 EPOCHS = 100
 VOC_SIZE = vocab_size
-LEARNING_RATE = 0.001 #0.0001
+LEARNING_RATE = 0.001
 
 print('\nSettings:\nEMBED_HIDDEN_SIZE: %d\nSTORY_HIDDEN_SIZE: %d\nQUERY_HIDDEN_SIZE: %d'
       '\nN_LAYERS: %d\nBATCH_SIZE: %d\nEPOCHS: %d\nVOC_SIZE: %d\nLEARNING_RATE: %f\n\n'
@@ -463,9 +488,7 @@ test_loader = DataLoader(dataset=test_dataset,batch_size=BATCH_SIZE, shuffle=Tru
 
 
 ## Initialize Model and Optimizer
-
-model = QAModel(VOC_SIZE, EMBED_HIDDEN_SIZE, STORY_HIDDEN_SIZE, QUERY_HIDDEN_SIZE, VOC_SIZE, N_LAYERS)
-#criterion = nn.CrossEntropyLoss()
+model = SentenceModel(VOC_SIZE, EMBED_HIDDEN_SIZE, STORY_HIDDEN_SIZE, QUERY_HIDDEN_SIZE, VOC_SIZE, N_LAYERS)
 criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
@@ -473,7 +496,6 @@ print(model)
 
 
 ## Start training
-
 start = time.time()
 if PRINT_LOSS:
     print("Training for %d epochs..." % EPOCHS)
@@ -498,19 +520,17 @@ for epoch in range(1, EPOCHS + 1):
     test_loss_history = test_loss_history+test_loss
 
     # Add Loss to history
-    train_acc_history.append(train_accuracy) # = train_acc_history + [train_accuracy]
-    test_acc_history.append(test_accuracy) # = test_acc_history + test_accuracy
+    train_acc_history.append(train_accuracy)
+    test_acc_history.append(test_accuracy)
 
 # Plot Loss
 if PLOT_LOSS:
     plt.figure()
-    plt.hold(True)
     plt.plot(train_loss_history,'b')
     plt.plot(test_loss_history,'r')
     plt.show()
 
     plt.figure()
-    plt.hold(True)
     plt.plot(train_acc_history,'b')
     plt.plot(test_acc_history,'r')
     plt.show()
