df51c894b9e50fb6028b6dfa13c3d0c3c5728963
pwieler <pwieler@users.noreply.github.com>
2018-01-21 23:20:14 +0100

    restructuring for better overview

--- DeepQA.py
+++ DeepQA.py
@@ -1,179 +1,14 @@
 from __future__ import print_function
 from functools import reduce
-import re
-import numpy as np
 import matplotlib.pyplot as plt
-import math
 import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
+from preprocessing.preprocessing import time_since, get_stories, vectorize_stories
 
-
-## Preprocessing of Babi-Data --> this just works for the Sentence-Model!
-
-def time_since(since):
-    s = time.time() - since
-    m = math.floor(s / 60)
-    s -= m * 60
-    return '%dm %ds' % (m, s)
-
-def pad_sequences(sequences, maxlen=None, dtype='int32',
-                  padding='pre', truncating='pre', value=0.):
-    """Pads each sequence to the same length (length of the longest sequence).
-    If maxlen is provided, any sequence longer
-    than maxlen is truncated to maxlen.
-    Truncation happens off either the beginning (default) or
-    the end of the sequence.
-    Supports post-padding and pre-padding (default).
-    # Arguments
-        sequences: list of lists where each element is a sequence
-        maxlen: int, maximum length
-        dtype: type to cast the resulting sequence.
-        padding: 'pre' or 'post', pad either before or after each sequence.
-        truncating: 'pre' or 'post', remove values from sequences larger than
-            maxlen either in the beginning or in the end of the sequence
-        value: float, value to pad the sequences to the desired value.
-    # Returns
-        x: numpy array with dimensions (number_of_sequences, maxlen)
-    # Raises
-        ValueError: in case of invalid values for `truncating` or `padding`,
-            or in case of invalid shape for a `sequences` entry.
-    """
-    if not hasattr(sequences, '__len__'):
-        raise ValueError('`sequences` must be iterable.')
-    lengths = []
-    for x in sequences:
-        if not hasattr(x, '__len__'):
-            raise ValueError('`sequences` must be a list of iterables. '
-                             'Found non-iterable: ' + str(x))
-        lengths.append(len(x))
-
-    num_samples = len(sequences)
-    if maxlen is None:
-        maxlen = np.max(lengths)
-
-    # take the sample shape from the first non empty sequence
-    # checking for consistency in the main loop below.
-    sample_shape = tuple()
-    for s in sequences:
-        if len(s) > 0:
-            sample_shape = np.asarray(s).shape[1:]
-            break
-
-    x = (np.ones((num_samples, maxlen) + sample_shape) * value).astype(dtype)
-    for idx, s in enumerate(sequences):
-        if not len(s):
-            continue  # empty list/array was found
-        if truncating == 'pre':
-            trunc = s[-maxlen:]
-        elif truncating == 'post':
-            trunc = s[:maxlen]
-        else:
-            raise ValueError('Truncating type "%s" not understood' % truncating)
-
-        # check `trunc` has expected shape
-        trunc = np.asarray(trunc, dtype=dtype)
-        if trunc.shape[1:] != sample_shape:
-            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
-                             (trunc.shape[1:], idx, sample_shape))
-
-        if padding == 'post':
-            x[idx, :len(trunc)] = trunc
-        elif padding == 'pre':
-            x[idx, -len(trunc):] = trunc
-        else:
-            raise ValueError('Padding type "%s" not understood' % padding)
-    return x
-
-def parse_stories(lines, only_supporting=False):
-    '''Parse stories provided in the bAbi tasks format
-
-    If only_supporting is true,
-    only the sentences that support the answer are kept.
-    '''
-    data = []
-    story = []
-    for line in lines:
-        line = line.strip()
-        nid, line = line.split(' ', 1)
-        nid = int(nid)
-        if nid == 1:
-            story = []
-        if '\t' in line:
-            q, a, supporting = line.split('\t')
-            q = tokenize(q)
-            substory = None
-            if only_supporting:
-                # Only select the related substory
-                supporting = map(int, supporting.split())
-                substory = [story[i - 1] for i in supporting]
-            else:
-                # Provide all the substories
-                substory = [x for x in story if x]
-            data.append((substory, q, a))
-            story.append('')
-        else:
-            sent = tokenize(line)
-            story.append(sent)
-    return data
-
-def get_stories(f, only_supporting=False, max_length=None):
-    '''Given a file name, read the file, retrieve the stories,
-    and then convert the sentences into a single story.
-
-    If max_length is supplied,
-    any stories longer than max_length tokens will be discarded.
-    '''
-    data = parse_stories(f.readlines(), only_supporting=only_supporting)
-    #flatten = lambda data: reduce(lambda x, y: x + y, data)
-    for story, q, answer in data:
-        data1 = [(story, q, answer) for story, q, answer in data if not max_length or len(story) <= max_length]
-        data2 = [(story[len(story)-max_length:len(story)], q, answer) for story, q, answer in data if not max_length or len(story) > max_length]
-        #if not max_length or len(story) <= max_length:
-        #    data.append((story, q, answer))
-        #else:
-        #    data.append((story[len(story)-max_length:len(story)], q, answer))
-    return data1+data2
-
-def tokenize(sent):
-    '''Return the tokens of a sentence including punctuation.
-
-    >>> tokenize('Bob dropped the apple. Where is the apple?')
-    ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
-    '''
-    return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
-
-def vectorize_stories(data, word_idx, story_maxlen, query_maxlen, fact_maxlen):
-    xs = []
-    xqs = []
-    ys = []
-    facts_lengths = []
-    for stories, query, answer in data:
-        xf = []
-        for story in stories:
-            x = [word_idx[w] for w in story]
-            xf.append(x)
-
-        xfl = [len(l) for l in xf]
-        facts_lengths.append(np.array(xfl))
-        xf = pad_sequences(xf, maxlen=fact_maxlen, padding='post')
-
-        xq = [word_idx[w] for w in query]
-        # let's not forget that index 0 is reserved
-        #y = np.zeros(len(word_idx) + 1)
-        #y[word_idx[answer]] = 1
-        #no one-hot-encoding for answer anymore!!
-        y = word_idx[answer]
-        xs.append(xf)
-        xqs.append(xq)
-        ys.append(y)
-    xsl = [len(l) for l in xs]  #contains length of stories
-    xqsl = [len(l) for l in xqs] # contains length of queries
-
-    return pad_sequences(xs, maxlen=story_maxlen, padding='post'), pad_sequences(xqs, maxlen=query_maxlen, padding='post'), np.array(ys), np.array(xsl), np.array(xqsl), pad_sequences(facts_lengths, maxlen=story_maxlen, padding='post') # info pad_sequence wurde in rnn.py reinkopiert
 
 class QADataset(Dataset):
 
@@ -242,8 +77,6 @@ class SentenceModel(nn.Module):
         self.softmax = nn.LogSoftmax()
 
     def forward(self, story, query, story_lengths, query_lengths, fact_lengths):
-
-
         #story: BATCH_SIZE x STORY_MAX_LEN x FACT_MAX_LEN
         #query: BATCH_SIZE x QUERY_MAX_LEN
         #story_lengths: contains the number of facts each story has
@@ -380,7 +213,7 @@ def train():
     return train_loss_history, accuracy, total_loss  # loss per epoch
 
 
-
+# Test-Cycle
 def test():
 
     model.eval()
@@ -426,28 +259,20 @@ def test():
 
 
 ## Load data
-
 data_path = "data/"
-
-#challenge = 'tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
 challenge = 'tasks_1-20_v1-2/en/qa2_two-supporting-facts_{}.txt'
-#challenge = 'tasks_1-20_v1-2/en/qa3_three-supporting-facts_{}.txt'
-#challenge = 'tasks_1-20_v1-2/en/qa6_yes-no-questions_{}.txt'
-
-print(challenge)
 
 train_data = get_stories(open(data_path + challenge.format('train'), 'r'), max_length=20)
 test_data = get_stories(open(data_path + challenge.format('test'), 'r'), max_length=20)
 
 ## Preprocess data
-
 vocab = set()
 flatten = lambda data: reduce(lambda x, y: x + y, data)
 for story, q, answer in train_data + test_data:
     vocab |= set(flatten(story) + q + [answer])
 vocab = sorted(vocab)
 
-# Reserve 0 for masking via pad_sequences
+
 # Vocabluary Size
 vocab_size = len(vocab) + 1
 # Creates Dictionary
@@ -461,8 +286,10 @@ fact_maxlen = 7
 ## Parameters
 EMBED_HIDDEN_SIZE = 50
 STORY_HIDDEN_SIZE = 50
-QUERY_HIDDEN_SIZE = 50  # note: since we are adding the encoded query to the embedded stories,
+QUERY_HIDDEN_SIZE = 50
+# note: since we are adding the encoded query to the embedded stories,
 #  QUERY_HIDDEN_SIZE should be equal to EMBED_HIDDEN_SIZE
+
 N_LAYERS = 1
 BATCH_SIZE = 32
 EPOCHS = 100
---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,168 @@
+from __future__ import print_function
+import re
+import numpy as np
+import math
+import time
+
+## Preprocessing of Babi-Data --> this preprocessing just works for the Sentence-Model!
+
+def time_since(since):
+    s = time.time() - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def pad_sequences(sequences, maxlen=None, dtype='int32',
+                  padding='pre', truncating='pre', value=0.):
+    """Pads each sequence to the same length (length of the longest sequence).
+    If maxlen is provided, any sequence longer
+    than maxlen is truncated to maxlen.
+    Truncation happens off either the beginning (default) or
+    the end of the sequence.
+    Supports post-padding and pre-padding (default).
+    # Arguments
+        sequences: list of lists where each element is a sequence
+        maxlen: int, maximum length
+        dtype: type to cast the resulting sequence.
+        padding: 'pre' or 'post', pad either before or after each sequence.
+        truncating: 'pre' or 'post', remove values from sequences larger than
+            maxlen either in the beginning or in the end of the sequence
+        value: float, value to pad the sequences to the desired value.
+    # Returns
+        x: numpy array with dimensions (number_of_sequences, maxlen)
+    # Raises
+        ValueError: in case of invalid values for `truncating` or `padding`,
+            or in case of invalid shape for a `sequences` entry.
+    """
+    if not hasattr(sequences, '__len__'):
+        raise ValueError('`sequences` must be iterable.')
+    lengths = []
+    for x in sequences:
+        if not hasattr(x, '__len__'):
+            raise ValueError('`sequences` must be a list of iterables. '
+                             'Found non-iterable: ' + str(x))
+        lengths.append(len(x))
+
+    num_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    # take the sample shape from the first non empty sequence
+    # checking for consistency in the main loop below.
+    sample_shape = tuple()
+    for s in sequences:
+        if len(s) > 0:
+            sample_shape = np.asarray(s).shape[1:]
+            break
+
+    x = (np.ones((num_samples, maxlen) + sample_shape) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if not len(s):
+            continue  # empty list/array was found
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError('Truncating type "%s" not understood' % truncating)
+
+        # check `trunc` has expected shape
+        trunc = np.asarray(trunc, dtype=dtype)
+        if trunc.shape[1:] != sample_shape:
+            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
+                             (trunc.shape[1:], idx, sample_shape))
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError('Padding type "%s" not understood' % padding)
+    return x
+
+def parse_stories(lines, only_supporting=False):
+    '''Parse stories provided in the bAbi tasks format
+
+    If only_supporting is true,
+    only the sentences that support the answer are kept.
+    '''
+    data = []
+    story = []
+    for line in lines:
+        line = line.strip()
+        nid, line = line.split(' ', 1)
+        nid = int(nid)
+        if nid == 1:
+            story = []
+        if '\t' in line:
+            q, a, supporting = line.split('\t')
+            q = tokenize(q)
+            substory = None
+            if only_supporting:
+                # Only select the related substory
+                supporting = map(int, supporting.split())
+                substory = [story[i - 1] for i in supporting]
+            else:
+                # Provide all the substories
+                substory = [x for x in story if x]
+            data.append((substory, q, a))
+            story.append('')
+        else:
+            sent = tokenize(line)
+            story.append(sent)
+    return data
+
+def get_stories(f, only_supporting=False, max_length=None):
+    '''Given a file name, read the file, retrieve the stories,
+    and then convert the sentences into a single story.
+
+    If max_length is supplied,
+    any stories longer than max_length tokens will be discarded.
+    '''
+    data = parse_stories(f.readlines(), only_supporting=only_supporting)
+    #flatten = lambda data: reduce(lambda x, y: x + y, data)
+    for story, q, answer in data:
+        data1 = [(story, q, answer) for story, q, answer in data if not max_length or len(story) <= max_length]
+        data2 = [(story[len(story)-max_length:len(story)], q, answer) for story, q, answer in data if not max_length or len(story) > max_length]
+        #if not max_length or len(story) <= max_length:
+        #    data.append((story, q, answer))
+        #else:
+        #    data.append((story[len(story)-max_length:len(story)], q, answer))
+    return data1+data2
+
+def tokenize(sent):
+    '''Return the tokens of a sentence including punctuation.
+
+    >>> tokenize('Bob dropped the apple. Where is the apple?')
+    ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
+    '''
+    return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
+
+def vectorize_stories(data, word_idx, story_maxlen, query_maxlen, fact_maxlen):
+    xs = []
+    xqs = []
+    ys = []
+    facts_lengths = []
+    for stories, query, answer in data:
+        xf = []
+        for story in stories:
+            x = [word_idx[w] for w in story]
+            xf.append(x)
+
+        xfl = [len(l) for l in xf]
+        facts_lengths.append(np.array(xfl))
+        xf = pad_sequences(xf, maxlen=fact_maxlen, padding='post')
+
+        xq = [word_idx[w] for w in query]
+        # let's not forget that index 0 is reserved
+        #y = np.zeros(len(word_idx) + 1)
+        #y[word_idx[answer]] = 1
+        #no one-hot-encoding for answer anymore!!
+        y = word_idx[answer]
+        xs.append(xf)
+        xqs.append(xq)
+        ys.append(y)
+    xsl = [len(l) for l in xs]  #contains length of stories
+    xqsl = [len(l) for l in xqs] # contains length of queries
+
+    return pad_sequences(xs, maxlen=story_maxlen, padding='post'), pad_sequences(xqs, maxlen=query_maxlen, padding='post'), np.array(ys), np.array(xsl), np.array(xqsl), pad_sequences(facts_lengths, maxlen=story_maxlen, padding='post') # info pad_sequence wurde in rnn.py reinkopiert
